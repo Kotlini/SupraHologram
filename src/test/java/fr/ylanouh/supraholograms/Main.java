ea3313fe99536237e8ae2cb61c7f4c4fc0f2b76b
Ylanouh <ylanouh.dev@gmail.com>
2023-04-02 15:34:31 +0200

    add

--- src/test/java/fr/ylanouh/supraholograms/Main.java
+++ src/test/java/fr/ylanouh/supraholograms/Main.java
@@ -1,7 +1,11 @@
 package fr.ylanouh.supraholograms;
 
 import fr.ylanouh.supraholograms.commands.TestCmd;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public class Main extends JavaPlugin {
 
@@ -11,6 +15,21 @@ public class Main extends JavaPlugin {
         instance = this;
 
         getCommand("test").setExecutor(new TestCmd());
+
+        System.out.println("SupraHolograms was enabled");
+
+        System.out.println("Unit test starting...");
+        try {
+            System.out.println("Starting test at 0, 150, 0");
+            SupraHologramsTests.prepare(new Location(Bukkit.getWorld("world"), 0, 150, 0));
+            Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), ()->{
+                new SupraHologramsTests().testAll();
+                System.out.println("Tests success");
+            });
+        } catch (Exception e) {
+            System.out.println("Tests failed with errors");
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
 
     @Override
