f6ccdbcc9841877e1b1ac96ea8c7b6ce8a800668
Ylanouh <ylanouh.dev@gmail.com>
2023-04-02 14:37:11 +0200

    change message test unit

--- src/test/java/fr/ylanouh/supraholograms/commands/TestCmd.java
+++ src/test/java/fr/ylanouh/supraholograms/commands/TestCmd.java
@@ -20,15 +20,17 @@ public class TestCmd implements CommandExecutor {
         else location = new Location(Bukkit.getWorld("world"), 0, 150, 0);
 
         try {
-            sender.sendMessage("Starting test at your position");
+            sender.sendMessage("Starting test at " + location.getX() + ", " + location.getY() + ", " + location.getX());
             SupraHologramsTests.prepare(location);
             Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), ()->{
                 new SupraHologramsTests().testAll();
                 sender.sendMessage("Tests success");
+                System.out.println("Tests success");
             });
         } catch (Exception e) {
-            sender.sendMessage("Tests failed with errors: " + e);
+            sender.sendMessage("Tests failed with errors: ");
             sender.sendMessage(Arrays.toString(e.getStackTrace()));
+            System.out.println("Tests failed with errors");
         }
 
         return false;
